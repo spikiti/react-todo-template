0a48ef87de88694f14d47801b3fb648f84fd50a5
Badr <badr@fastmail.com>
2023-05-26 21:54:34 +0200

    create useTodo hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,33 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import { useMemo, useState } from "react";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
-import { Todo, Filter } from "./types";
+import { Filter } from "./types";
+import { useTodo } from "./useTodo";
 
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const { todos, addTodo, completeTodo, deleteTodo, completeAllTodos } =
+    useTodo();
+
   const [filter, setFilter] = useState<Filter>("ALL");
 
-  const displayedTodos = todos.filter((t) =>
-    filter === "ALL"
-      ? true
-      : filter === "COMPLETED"
-      ? t.status === "completed"
-      : t.status === "pending"
+  const pendingTodosCount = useMemo(
+    () => todos.filter((t) => t.status === "pending").length,
+    [todos]
   );
 
-  const addTodo = (text: string) => {
-    setTodos([...todos, { id: nanoid(), text, status: "pending" }]);
-  };
-
-  const completeTodo = (id: string) => {
-    setTodos(
-      todos.map((t) =>
-        t.id === id
-          ? { ...t, status: t.status === "completed" ? "pending" : "completed" }
-          : t
-      )
-    );
-  };
-
-  const deleteTodo = (id: string) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
-
-  const completeAllTodos = () => {
-    const hasPending = todos.some((t) => t.status === "pending");
-
-    setTodos(
-      todos.map((t) => ({
-        ...t,
-        status: hasPending ? "completed" : "pending",
-      }))
-    );
-  };
-
-  const pendingTodosCount = todos.filter((t) => t.status === "pending").length;
+  const displayedTodos = useMemo(
+    () =>
+      todos.filter((t) =>
+        filter === "ALL"
+          ? true
+          : filter === "COMPLETED"
+          ? t.status === "completed"
+          : t.status === "pending"
+      ),
+    [filter, todos]
+  );
 
   return (
     <div className="flex flex-col gap-2">
---
+++ src/useTodo.ts
@@ -0,0 +1,42 @@
+import { useCallback, useState } from "react";
+import { nanoid } from "nanoid";
+
+import { Todo } from "./types";
+
+export const useTodo = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = useCallback((text: string) => {
+    setTodos((todos) => [...todos, { id: nanoid(), text, status: "pending" }]);
+  }, []);
+
+  const completeTodo = useCallback((id: string) => {
+    setTodos((todos) =>
+      todos.map((t) =>
+        t.id === id
+          ? {
+              ...t,
+              status: t.status === "completed" ? "pending" : "completed",
+            }
+          : t
+      )
+    );
+  }, []);
+
+  const deleteTodo = useCallback((id: string) => {
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+  }, []);
+
+  const completeAllTodos = useCallback(() => {
+    setTodos((todos) => {
+      const hasPending = todos.some((t) => t.status === "pending");
+
+      return todos.map((t) => ({
+        ...t,
+        status: hasPending ? "completed" : "pending",
+      }));
+    });
+  }, []);
+
+  return { todos, addTodo, completeTodo, deleteTodo, completeAllTodos };
+};
